64eb83d2271d263dd55dcf425045185bd3dc7296
edivaldomonteiro <edivaldofcr@gmail.com>
2020-06-26 23:45:59 -0300

    Atributos inclusos

--- oo/pesoa.py
+++ oo/pesoa.py
@@ -1,2 +1,14 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=39):
+        self.idade = idade
+        self.nome = nome
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa('Edivaldo')
+    print(p.cumprimentar())
+    print(id(p))
+    print(p.nome, p.idade)
+
