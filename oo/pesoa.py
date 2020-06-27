e742f7e85cb5d9cc9b6f079772c8abc0415f46b3
edivaldomonteiro <edivaldofcr@gmail.com>
2020-06-26 23:58:08 -0300

    Adicionado atributo complexo

--- oo/pesoa.py
+++ oo/pesoa.py
@@ -1,14 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=39):
+    def __init__(self, *filhos, nome=None, idade=18):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Edivaldo')
-    print(p.cumprimentar())
-    print(id(p))
-    print(p.nome, p.idade)
+    jenny = Pessoa(nome='Jenny')
+    bre = Pessoa(nome='Bendon')
+    edi = Pessoa(jenny, bre, nome='Edivaldo', idade=39)
+    print(edi.cumprimentar())
+    print(id(edi))
+    print(edi.nome, edi.idade)
+    print(edi.filhos[0].nome)
+    for filhos in edi.filhos:
+        print(filhos.nome)
 
